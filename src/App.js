140431d496a2a4673332eb2c7c014775c150ac59
EmersonDjob <jobbr70@gmail.com>
2021-07-17 00:54:11 -0300

    testes nas props do componente

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        
-        <FloatingWhatsApp phoneNumber={5567999468759} />
+        <FloatingWhatsApp phoneNumber={5567999468759} accountName="Brazil Solar" statusMessage="Bem vindo" chatMessage="Gostaria de fazer um orçamento conosco"   />
       </header>
     </div>
   );
